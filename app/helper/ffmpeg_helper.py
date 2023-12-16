4aef80e86704d0afc8cc1fcc3ff40e9b10564ea1
yujianhui21 <1164615132@qq.com>
2023-12-16 16:37:20 +0800

    测试：支持调用vaapi截图

--- app/helper/ffmpeg_helper.py
+++ app/helper/ffmpeg_helper.py
@@ -1,10 +1,30 @@
 import json
 import subprocess
 
+import log
 from app.utils import SystemUtils
 
 
 class FfmpegHelper:
+
+    @staticmethod
+    def has_vaapi():
+        """
+        判断系统是否支持 VA-API（核显加速）
+        """
+        try:
+            # 运行 ls -l /dev/dri 命令并捕获输出
+            result = subprocess.run(['ls', '-l', '/dev/dri'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            output = result.stdout + result.stderr
+
+            # 检查输出中是否包含相关设备信息，这里使用 renderD 作为判断标志
+            if 'renderD' in output:
+                return True
+            else:
+                return False
+
+        except subprocess.CalledProcessError:
+            return False
 
     @staticmethod
     def get_thumb_image_from_video(video_path, image_path, frames="00:03:01"):
@@ -13,9 +33,18 @@ class FfmpegHelper:
         """
         if not video_path or not image_path:
             return False
-        cmd = 'ffmpeg -i "{video_path}" -ss {frames} -vframes 1 -f image2 "{image_path}"'.format(video_path=video_path,
-                                                                                                 frames=frames,
-                                                                                                 image_path=image_path)
+
+        if FfmpegHelper.has_vaapi():
+            # 使用核显加速
+            cmd = 'ffmpeg -init_hw_device vaapi=/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi ' \
+                  '-i "{video_path}" -ss {frames} -vframes 1 -vf "format=nv12|vaapi,hwupload" ' \
+                  '-f image2 "{image_path}"'.format(video_path=video_path, frames=frames, image_path=image_path)
+        else:
+            # 没有核显加速
+            cmd = 'ffmpeg -i "{video_path}" -ss {frames} -vframes 1 -f image2 "{image_path}"'.format(
+                video_path=video_path, frames=frames, image_path=image_path)
+        log.info("【FfmpegHelper】cmd：%s " % cmd)
+        log.info("【FfmpegHelper】是否调用vaapi：%s " % FfmpegHelper.has_vaapi())
         result = SystemUtils.execute(cmd)
         if result:
             return True
